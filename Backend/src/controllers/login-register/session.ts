62d31ecc24497bca9fa220849aba6f94a79de5ab
UsopKing7 <nicolas@gmail.com@gmail.com>
2025-06-10 13:51:53 -0400

    implementa la lógica de inicio de sesión con validación de usuario y contraseña

--- Backend/src/controllers/login-register/session.ts
+++ Backend/src/controllers/login-register/session.ts
@@ -38,3 +38,40 @@ routerSession.post('/register', async (req: Request, res: Response) => {
     })
   }
 })
+
+routerSession.post('/login', async (req: Request, res: Response) => {
+  try {
+    const vLogin: Register = validacionRegister.parse(req.body)
+    const [usuarioExiste] = await pool.query<UsuarioConsulta[]>(
+      'SELECT * FROM usuarios WHERE username = ?',
+      [vLogin.username]
+    )
+
+    if (usuarioExiste.length === 0) {
+      res.status(404).json({
+        message: 'El nombre de usuario no existe'
+      })
+      return
+    }
+
+    const usuario = usuarioExiste[0]
+
+    const verificarPassword = await bcypt.compare(vLogin.password, usuario.password)
+
+    if (!verificarPassword) {
+      res.status(404).json({
+        message: 'La contraseña o usuario incorrectos'
+      })
+      return
+    }
+
+    res.status(200).json({
+      message: 'Login exitoso'
+    })
+  } catch (error) {
+    res.status(500).json({
+      message: 'Error al iniciar session',
+      error: formatError(error)
+    })
+  }
+})
