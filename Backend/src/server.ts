ca85df5a7d80f56927046e637c43391c922ca754
UsopKing7 <nicolas@gmail.com@gmail.com>
2025-06-10 13:38:31 -0400

    ajusta,ps server para que pueda resivir json y las rutas, tambien mejora en el port env

--- Backend/src/server.ts
+++ Backend/src/server.ts
@@ -1,15 +1,13 @@
 import express from 'express'
+import { midelware } from './routers/rutasMidelwares'
+import { PORT } from './types'
 
 const app = express()
+app.use(express.json())
+app.use(midelware)
 
-app.get('/', (_req, res) => {
-  res.send('<h1>Hello world</>')
-})
-
-const PORT = process.env.PORT ?? 3333
-
-app.listen(PORT, () => {
+app.listen(PORT.port, '0.0.0.0', () => {
   console.table({
-    url: `http://0.0.0.0:${PORT}`
+    url: `http://0.0.0.0:${PORT.port}`
   })
 })
