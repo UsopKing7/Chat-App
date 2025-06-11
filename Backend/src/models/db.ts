341a16504a35aea656030cc010c973f8492d1861
UsopKing7 <nicolas@gmail.com@gmail.com>
2025-06-10 22:21:16 -0400

    fix: corrige la configuración del pool de conexión a la base de datos

--- Backend/src/models/db.ts
+++ Backend/src/models/db.ts
@@ -6,7 +6,7 @@ env.config()
 
 export const pool = createPool({
   host: process.env.DB_HOST,
-  pool: process.env.DB_PORT,
+  port: process.env.DB_PORT ? Number(process.env.DB_PORT) : undefined,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME
